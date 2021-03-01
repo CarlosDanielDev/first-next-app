552189da7e6ad4d7e301cd6efec35614bbb1f38d
Carlos Daniel <killer.dk20@gmail.com>
2021-03-01 15:12:19 -0300

    feat: #5 - Use homepage style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import * as S from '../styles/pages/Home';
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello Next!</h1>
+      <S.Title>Hello Next!</S.Title>
     </div>
   )
 }
